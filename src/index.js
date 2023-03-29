7e0926f0ddf7d6488193772b03f7f7a2349743c2
Yilikal250 <teshomeyilikal250@gmail.com>
2023-03-29 15:31:20 +0300

    creatred a boiler plate

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Greetings(){
+  return <h4>this is Yilikal and this is my first </h4>;
+}
+
+ReactDOM.render(<Greetings/>,document.getElementById('root')); 
