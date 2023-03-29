67c621b6511b3091e56dc0aa0c2fe074bed37ff8
Yilikal250 <teshomeyilikal250@gmail.com>
2023-03-29 17:41:03 +0300

    components and css added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import './index.css'
 
 // 1,
 //stateless function component
@@ -14,16 +14,33 @@ import ReactDOM from 'react-dom';
 //className instead of class
 //formating
 
-function Greetings(){
-  return (<div>  
-  <h4>this is Yilikal and this is my first 
-    </h4> 
-    </div>);
+// Nested Components,React Tools 
+
+function BookList(){
+  return (
+    <section>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    </section>
+  );
 }
 
-// const Greetings = () => {
 
-//   return React.createElement('h1',{},'hello world')
-// }
+const Book = () => {
 
-ReactDOM.render(<Greetings/>,document.getElementById('root')); 
+  return <article>
+    <Image></Image>
+    <Title/>
+    <Author/>
+     </article>;
+}
+
+const Image = () => <img src="https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UY327_FMwebp_QL65_.jpg" alt=""/>
+const Title = () => <h1>I Love You to the Moon and Back</h1>;
+const Author = () => <h4>Amelia Hepworth</h4>
+
+ReactDOM.render(<BookList/>,document.getElementById('root')); 
