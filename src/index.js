cf8506d248e2e744fe6e1835fb154a6dee03c83e
Yilikal250 <teshomeyilikal250@gmail.com>
2023-03-29 16:00:05 +0300

    Jsx rules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+// 1,
+//stateless function component
+//always return jsx
+
+//2,
+//jsx rules
+//return single element
+//div / section / article or Fregment
+//use camelCase for html attribute
+//className instead of class
+//formating
+
 function Greetings(){
-  return <h4>this is Yilikal and this is my first </h4>;
+  return (<div>  
+  <h4>this is Yilikal and this is my first 
+    </h4> 
+    </div>);
 }
+
+// const Greetings = () => {
+
+//   return React.createElement('h1',{},'hello world')
+// }
 
 ReactDOM.render(<Greetings/>,document.getElementById('root')); 
