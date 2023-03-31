f90baaf130beba5a9fd9838143fab9ff97fa8b0f
Yilikal250 <teshomeyilikal250@gmail.com>
2023-03-31 14:39:22 +0300

    components and probs added

--- src/index.js
+++ src/index.js
@@ -2,45 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 
-// 1,
-//stateless function component
-//always return jsx
+const  books = [{
+  id:1, 
+  img:'https://m.media-amazon.com/images/I/81TMvHnKlwL._AC_UY327_FMwebp_QL65_.jpg',
+  title:'I Love You to the Moon and Back',
+  author:'Sam McBratney and Anita Jeram'
+},
 
-//2,
-//jsx rules
-//return single element
-//div / section / article or Fregment
-//use camelCase for html attribute
-//className instead of class
-//formating
+ {
+  id:2,
+  img:'https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UY327_FMwebp_QL65_.jpg',
+  title:'Guess How Much I Love You',
+  author:'Amelia Hepworth'
+},
+{
+  id:3,
+  img:'https://m.media-amazon.com/images/I/71nfLYoh5jL._AC_UY327_FMwebp_QL65_.jpg',
+  title:'Peter Rabbit I Love You Daddy',
+  author:'Beatrix Potter'
+}
+];
 
-// Nested Components,React Tools 
 
 function BookList(){
   return (
-    <section>
-    <Book/>
-    <Book/>
-    <Book/>
-    <Book/>
-    <Book/>
-    <Book/>
+    <section className='booklist'>
+      {books.map((books)=> {
+      return <Book key={books.id} {...books}> </Book>;
+    })}
+
     </section>
   );
 }
 
 
-const Book = () => {
 
-  return <article>
-    <Image></Image>
-    <Title/>
-    <Author/>
+const Book = (props) => {
+  const {img,title,author} = props;
+  return <article className='book'>
+    <img src={img} alt=""/>
+    <h1>{title}</h1>
+    <h4>{author}</h4>
      </article>;
 }
 
-const Image = () => <img src="https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UY327_FMwebp_QL65_.jpg" alt=""/>
-const Title = () => <h1>I Love You to the Moon and Back</h1>;
-const Author = () => <h4>Amelia Hepworth</h4>
 
 ReactDOM.render(<BookList/>,document.getElementById('root')); 
